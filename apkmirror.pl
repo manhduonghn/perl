6e54620761c9b42bace80c2a584ac29f555093e3
manhduonghn <manhduonghn@live.com>
2024-05-17 15:34:02 +0700

    Create apkmirror.pl

---
+++ apkmirror.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use JSON;
+use File::Temp qw(tempfile);
+
+sub req {
+    my ($url, $output) = @_;
+
+    my $headers = join(' ',
+        '--header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0"',
+        '--header="Content-Type: application/octet-stream"',
+        '--header="Accept-Language: en-US,en;q=0.9"',
+        '--header="Connection: keep-alive"',
+        '--header="Upgrade-Insecure-Requests: 1"',
+        '--header="Cache-Control: max-age=0"',
+        '--header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"'
+    );
+
+    my $command = "wget $headers --keep-session-cookies --timeout=30 -nv -O \"$output\" \"$url\"";
+    system($command) == 0
+        or die "Failed to execute $command: $?";
+}

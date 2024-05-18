1f158fa61161ef2eba8e498573f61f447fb26e5a
manhduonghn <manhduonghn@live.com>
2024-05-18 22:15:53 +0700

    Update apkpure.pl

--- apkpure.pl
+++ apkpure.pl
@@ -71,7 +71,7 @@ sub apkpure {
 
         my @version;
         for my $line (@lines) {
-            if ($line =~ /.*data-dt-version="(.*?)"/) {
+            if ($line =~ /.*data-dt-version="(.*?)".*/) {
                 $version = "$1";
             }
         }

38fe5324491f5b77d90ea78ea138714708fbf445
manhduonghn <manhduonghn@live.com>
2024-05-18 15:08:17 +0700

    Update apkmirror.pl

--- apkmirror.pl
+++ apkmirror.pl
@@ -1,12 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use JSON;
 use File::Temp qw(tempfile);
 
 sub req {
     my ($url, $output) = @_;
-
     my $headers = join(' ',
         '--header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0"',
         '--header="Content-Type: application/octet-stream"',
@@ -23,10 +21,18 @@ sub req {
 }
 
 sub apkmirror {
-    my $version = "19.11.43"
+    my $version = "19.11.43";  # Corrected missing semicolon
     my $url = "https://www.apkmirror.com/apk/google-inc/youtube/youtube-" . (join '-', split /\./, $version) . "-release";
+
+    # Create a temporary file to store the output
     my ($fh, $tempfile) = tempfile();
+
+    # Fetch the URL and store the output in the temporary file
     req($url, $tempfile);
+
+    # Optional: Output the location of the temporary file
+    print "Downloaded content saved to $tempfile\n";
 }
 
+# Execute the apkmirror subroutine
 apkmirror();

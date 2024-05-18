9d6a2185ae2aa5bcc251d8a1da4ff1765d45d278
manhduonghn <manhduonghn@live.com>
2024-05-18 22:45:35 +0700

    Create scraper.sh

---
+++ scraper.sh
@@ -0,0 +1,6 @@
+perl apkmirror.pl 'google-inc' 'youtube-music' 'com.google.android.apps.youtube.music' 'arm64-v8a'
+perl apkmirror.pl 'google-inc' 'youtube' 'com.google.android.youtube'
+perl uptodown.pl 'youtube-music' 'com.google.android.apps.youtube.music'
+perl uptodown.pl 'youtube' 'com.google.android.youtube'
+perl apkpure.pl 'youtube-music' 'com.google.android.apps.youtube.music'
+perl apkpure.pl 'youtube' 'com.google.android.youtube'

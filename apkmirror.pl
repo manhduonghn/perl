d63ddc39a0b8360df1b26eada3af034b326addd0
manhduonghn <manhduonghn@live.com>
2024-05-18 19:39:27 +0700

    Update apkmirror.pl

--- apkmirror.pl
+++ apkmirror.pl
@@ -21,7 +21,6 @@ sub req {
         or die "Failed to execute $command: $?";
 }
 
-# Function to filter lines based on pattern and buffer size
 sub filter_lines {
     my ($pattern, $size, $buffer_ref) = @_;
     my @temp_buffer;
@@ -38,30 +37,23 @@ sub get_supported_version {
     my $pkg_name = shift;
     my $filename = 'patches.json';
     
-    open(my $fh, '<', $filename);
-    local $/;  # Slurp mode
+    open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
+    local $/; 
     my $json_text = <$fh>;
     close($fh);
 
     my $data = decode_json($json_text);
-    
-    # Initialize an empty set to hold versions
     my %versions;
 
-    # Iterate over each patch in the JSON data
     foreach my $patch (@{$data}) {
         my $compatible_packages = $patch->{'compatiblePackages'};
     
-        # Check if compatiblePackages is a non-empty list
-        if ($compatible_packages && ref($compatible_packages) eq 'ARRAY') {
-            # Iterate over each package in compatiblePackages
+            if ($compatible_packages && ref($compatible_packages) eq 'ARRAY') {
             foreach my $package (@$compatible_packages) {
-                # Check if package name and versions list is not empty
                 if (
                     $package->{'name'} eq $pkg_name &&
                     $package->{'versions'} && ref($package->{'versions'}) eq 'ARRAY' && @{$package->{'versions'}}
                 ) {
-                    # Add versions to the set
                     foreach my $version (@{$package->{'versions'}}) {
                         $versions{$version} = 1;
                     }
@@ -69,10 +61,7 @@ sub get_supported_version {
             }
         }
     }
-
-    # Sort versions in reverse order and get the latest version
     my $version = (sort {$b cmp $a} keys %versions)[0];
-
     return $version;
 }
 
@@ -81,9 +70,7 @@ sub apkmirror {
     $dpi ||= 'nodpi';
     $arch ||= 'universal';
 
-    # Create a temporary file to store the output
     my ($fh, $tempfile) = tempfile();
-
     my $version;
 
     if (my $supported_version = get_supported_version($package)) {
@@ -92,7 +79,6 @@ sub apkmirror {
         my $page = "https://www.apkmirror.com/uploads/?appcategory=$name";
         req($page, $tempfile);
 
-        # Read the temporary file content line by line
         open my $file_handle, '<', $tempfile or die "Could not open file '$tempfile': $!";
         my @lines = <$file_handle>;
         close $file_handle;
@@ -109,32 +95,21 @@ sub apkmirror {
 
         @versions = map { s/^\D+//; $_ } @versions;
         @versions = sort { version->parse($b) <=> version->parse($a) } @versions;
-
         $version = $versions[0];
-
         unlink $tempfile;
     }
 
     my $url = "https://www.apkmirror.com/apk/$org/$name/$name-" . (join '-', split /\./, $version) . "-release";
-
-    # Fetch the URL and store the output in the temporary file
     req($url, $tempfile);
 
-    # Read the temporary file content line by line
     open $fh, '<', $tempfile or die "Could not open file '$tempfile': $!";
     my @lines = <$fh>;
     close $fh;
 
-    # Step 1: Filter by dpi
     filter_lines(qr/>\s*$dpi\s*</, 16, \@lines);
-
-    # Step 2: Filter by arch
     filter_lines(qr/>\s*$arch\s*</, 14, \@lines);
-
-    # Step 3: Filter by APK
     filter_lines(qr/>\s*APK\s*</, 6, \@lines);
 
-    # Extract the download page URL
     my $download_page_url;
     my $i = 0;
     for my $line (@lines) {
@@ -143,18 +118,14 @@ sub apkmirror {
             last;
         }
     }
-
     unlink $tempfile;
     
-    # Fetch the download page and store the output in the temporary file
     req($download_page_url, $tempfile);
 
-    # Read the temporary file content again
     open $fh, '<', $tempfile or die "Could not open file '$tempfile': $!";
     @lines = <$fh>;
     close $fh;
 
-    # Extract final APK download URL from the content
     my $dl_apk_url;
     for my $line (@lines) {
         if ($line =~ /href="([^"]*key=[^"]*)"/) {
@@ -162,17 +133,14 @@ sub apkmirror {
             last;
         }
     }
-
-    unlink $tempfile;
+    unlink $tempfile;  
     
     req($dl_apk_url, $tempfile);
-
-    # Read the temporary file content again
+    
     open $fh, '<', $tempfile or die "Could not open file '$tempfile': $!";
     @lines = <$fh>;
     close $fh;
-
-    # Extract final APK download URL from the content
+    
     my $final_url;
     for my $line (@lines) {
         if ($line =~ /href="([^"]*key=[^"]*)"/) {
@@ -181,13 +149,12 @@ sub apkmirror {
             last;
         }
     }
-
-    unlink $tempfile;
+    unlink $tempfile;   
     
-    # Final download
     my $apk_filename = "$name-v$version.apk";
     req($final_url, $apk_filename);
 }
 
-# Execute the apkmirror subroutine
-apkmirror('google-inc', 'youtube-music', 'com.google.android.apps.youtube.music', 'arm64-v8a');
+# Main
+my ($org, $name, $package, $arch, $dpi) = @ARGV;
+apkmirror($org, $name, $package, $arch, $dpi);

c993aaf5b8b0448b796b6339cfbc741b15bec69a
manhduonghn <manhduonghn@live.com>
2024-05-18 21:58:06 +0700

    Update apkpure.pl

--- apkpure.pl
+++ apkpure.pl
@@ -77,7 +77,7 @@ sub apkpure {
                 last;
             }
         }
-        unlink $tempfile;
+       # unlink $tempfile;
     }
     
     req($url, $tempfile);
@@ -94,7 +94,7 @@ sub apkpure {
             last;
         }
     }
-    unlink $tempfile;
+    # unlink $tempfile;
     
     my $apk_filename = "$name-v$version.apk";
     req($final_url, $apk_filename);

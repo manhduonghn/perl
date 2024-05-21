c4def2eeac8bd4032ed25c2700f89371c4809688
manhduonghn <manhduonghn@live.com>
2024-05-21 23:35:32 +0700

    Delete scraper.sh

--- scraper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-perl github.pl
-perl apkmirror.pl 'google-inc' 'youtube-music' 'com.google.android.apps.youtube.music' 'arm64-v8a'
-perl apkmirror.pl 'google-inc' 'youtube' 'com.google.android.youtube'
-perl uptodown.pl 'youtube-music' 'com.google.android.apps.youtube.music'
-perl uptodown.pl 'youtube' 'com.google.android.youtube'
-perl apkpure.pl 'youtube-music' 'com.google.android.apps.youtube.music'
-perl apkpure.pl 'youtube' 'com.google.android.youtube'

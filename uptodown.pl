25b39d653f104b4993095dda3d8feb6b77f95a14
manhduonghn <manhduonghn@live.com>
2024-05-21 23:27:24 +0700

    Update uptodown.pl

--- uptodown.pl
+++ uptodown.pl
@@ -2,10 +2,10 @@
 use strict;
 use warnings;
 use JSON;
-use File::Temp qw(tempfile);
 
 sub req {
     my ($url, $output) = @_;
+    $output ||= '-';
     my $headers = join(' ',
         '--header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0"',
         '--header="Content-Type: application/octet-stream"',
@@ -16,9 +16,10 @@ sub req {
         '--header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"'
     );
 
-    my $command = "wget $headers --keep-session-cookies --timeout=30 -nv -O \"$output\" \"$url\"";
-    system($command) == 0
-        or die "Failed to execute $command: $?";
+    my $command = "wget $headers --keep-session-cookies --timeout=30 -nv -O $output \"$url\"";
+    my $content = `$command`;
+    die "Failed to execute $command: $?" if $? != 0;
+    return $content;
 }
 
 sub filter_lines {
@@ -48,7 +49,7 @@ sub get_supported_version {
     foreach my $patch (@{$data}) {
         my $compatible_packages = $patch->{'compatiblePackages'};
     
-            if ($compatible_packages && ref($compatible_packages) eq 'ARRAY') {
+        if ($compatible_packages && ref($compatible_packages) eq 'ARRAY') {
             foreach my $package (@$compatible_packages) {
                 if (
                     $package->{'name'} eq $pkg_name &&
@@ -68,36 +69,28 @@ sub get_supported_version {
 sub uptodown {
     my ($name, $package) = @_;
 
-    my ($fh, $tempfile) = tempfile();
     my $version;
 
     if (my $supported_version = get_supported_version($package)) {
         $version = $supported_version;
     } else {
         my $page = "https://$name.en.uptodown.com/android/versions";
-        req($page, $tempfile);
+        my $page_content = req($page);
 
-        open my $file_handle, '<', $tempfile or die "Could not open file '$tempfile': $!";
-        my @lines = <$file_handle>;
-        close $file_handle;
+        my @lines = split /\n/, $page_content;
 
-        my @version;
-        my $i = 0;
         for my $line (@lines) {
-            if ($line =~ /.*class="version">(.*?)<\/div>/ && ++$i == 1) {
+            if ($line =~ /.*class="version">(.*?)<\/div>/) {
                 $version = "$1";
                 last;
             }
         }
-        unlink $tempfile;
     }
 
     my $url = "https://$name.en.uptodown.com/android/versions";
-    req($url, $tempfile);
+    my $download_page_content = req($url);
 
-    open $fh, '<', $tempfile or die "Could not open file '$tempfile': $!";
-    my @lines = <$fh>;
-    close $fh;
+    my @lines = split /\n/, $download_page_content;
 
     filter_lines(qr/>\s*$version\s*<\/span>/, 5, \@lines);
     
@@ -109,13 +102,10 @@ sub uptodown {
             last;
         }
     }
-    unlink $tempfile;   
 
-    req($download_page_url, $tempfile);
+    my $final_page_content = req($download_page_url);
     
-    open $fh, '<', $tempfile or die "Could not open file '$tempfile': $!";
-    @lines = <$fh>;
-    close $fh;
+    @lines = split /\n/, $final_page_content;
     
     my $final_url;
     for my $line (@lines) {
@@ -124,7 +114,6 @@ sub uptodown {
             last;
         }
     }
-    unlink $tempfile;
     
     my $apk_filename = "$name-v$version.apk";
     req($final_url, $apk_filename);

5ea475b061868339133e7fa0c4af207189127131
manhduonghn <manhduonghn@live.com>
2024-05-23 09:13:44 +0700

    Update apkmirror.pm

--- utils/apkmirror.pm
+++ utils/apkmirror.pm
@@ -171,7 +171,7 @@ sub apkmirror {
 
     my $final_url;
     for my $line (@lines) {
-        if ($line =~ /href="(.*key=[^"]*\/)"/) {
+        if ($line =~ /href="(.*key=[^"]*)"/) {
             $final_url = $1; 
             unless ($final_url =~ /^https:\/\/www\.apkmirror\.com/) {
                 $final_url = "https://www.apkmirror.com$1";

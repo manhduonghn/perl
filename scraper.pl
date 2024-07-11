751dae0aad1084f059a7d5247aa394e8675cfb07
manhduonghn <manhduonghn@live.com>
2024-07-12 00:44:39 +0700

    Update scraper.pl

--- scraper.pl
+++ scraper.pl
@@ -11,8 +11,55 @@ use apkmirror qw(apkmirror);
 use github_downloader qw(download_resources);
 
 # Download Github releases assets 
-# download_resources("revanced");
+download_resources("revanced");
 
+# Apkmirror 
+apkmirror(
+    "google-inc", 
+    "youtube", 
+    "com.google.android.youtube"
+);
+undef $ENV{VERSION};
+
+# $ENV{VERSION} = "6.51.52";
+apkmirror(
+    "google-inc", 
+    "youtube-music", 
+    "com.google.android.apps.youtube.music", 
+    "arm64-v8a"
+);
+undef $ENV{VERSION};
+
+$ENV{VERSION} = "457.1.0.45.109";
+apkmirror(
+    "facebook-2", 
+    "messenger",
+    "",
+    "arm64-v8a"
+);
+undef $ENV{VERSION};
+
+# Apkpure 
+apkpure(
+    "youtube",
+    "com.google.android.youtube"
+);
+undef $ENV{VERSION};
+
+# $ENV{VERSION} = "6.51.52";
+apkpure(
+    "youtube-music"
+);
+undef $ENV{VERSION};
+
+# Uptodown
+uptodown(
+    "youtube",
+    "com.google.android.youtube"
+);
+undef $ENV{VERSION};
+
+#$ENV{VERSION} = "6.51.52";
 uptodown(
     "youtube-music"
 );
